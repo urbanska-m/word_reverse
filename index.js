b2e2f73ae24e59daab659261230dacd6b24b048c
Monika Urbanska <monika.urbanska1@gmail.com>
2020-05-17 17:35:51 +0100

    testing out first function, words reverse order

---
+++ index.js
@@ -0,0 +1,16 @@
+// reverse string
+// javascript has .reverse() method but only works on arrays
+// must first convert string to array
+
+// function reverseString(string) {
+//     let myArray = Array.from(string);
+//     let arrayReversed = myArray.reverse();
+//     // now need to convert it back into string
+//     // let arrayToString = arrayReversed.join();
+//     // returns a string but with commas
+
+//     let arrayToString = arrayReversed.join("")
+
+//     return arrayToString;
+// }
+// // doesn't work because reverses order of words
